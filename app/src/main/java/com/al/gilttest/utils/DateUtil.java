ed480eac61269976ad8bb610c0a2b24487b814f6
alexd3v <krintest1@gmail.com>
2017-11-07 01:02:41 +0200

    Add util to calculate period when sale will end

--- app/src/main/java/com/al/gilttest/utils/DateUtil.java
+++ app/src/main/java/com/al/gilttest/utils/DateUtil.java
@@ -1,44 +1,31 @@
 package com.al.gilttest.utils;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
+import org.joda.time.Days;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 
 /**
  * Provides method to calculate period when sale ends
  */
 public class DateUtil {
 
-    /* Fix that shit!!!!!!!!!!!!!!!!!!
-        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     */
+    private static LocalDateTime now = LocalDateTime.now();
+    private static String iso = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
-    private static String simpleDateFormatting = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-    private Date today = Calendar.getInstance().getTime();
 
     DateUtil() {
     }
 
-    /**
-     * Compare current date and date when sale finish
-     *
-     * @param end String value ISO-8601-formatted time for end of sale
-     * @return number of days
-     */
-    public static long interval(String end) {
-        DateFormat format = new SimpleDateFormat(simpleDateFormatting, Locale.getDefault());
-        long currentDate = System.currentTimeMillis();
-        long diff = 0;
-        try {
-            Date ends = format.parse(end);
-            diff = ends.getTime() - currentDate;
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+//    static boolean isDays();
+
+    public static int getDays(String s) {
+        return Days.daysBetween(now, format(s)).getDays();
+    }
+
+    private static LocalDateTime format(String s) {
+        DateTimeFormatter f = DateTimeFormat.forPattern(iso);
+        return f.parseLocalDateTime(s);
     }
 }
